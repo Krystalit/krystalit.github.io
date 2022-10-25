115ef3153cf57cdfe13f37c97e610af7b33e2482
Krystalit <111550717+Krystalit@users.noreply.github.com>
2022-10-25 15:09:53 +0800

    ver.Alpha 0.1.5

--- scripts/main.js
+++ scripts/main.js
@@ -18,39 +18,3 @@ var LnkSta = 0;
 Blink.onclick = function(){
   window.location.assign("https://www.bilibili.com");
 }
-
-MWlink.onclick = function(){
-  window.location.assign("https://www.merriam-webster.com/");
-}
-
-
-let rollImg = document.getElementById("rollimg");
-let rollImg2 = document.getElementById("rollimg2");
-
-function ShowB(){
-	Blink.style.display = "block";
-	MWlink.style.display = "none";
-}
-
-function ShowMW(){
-	Blink.style.display = "none";
-	MWlink.style.display = "block";
-}
-
-function topImage(){
-
-	if (LnkSta == 0){
-
-		ShowB;
-		var LnkSta = 1;}
-
-	else{
-
-		ShowMW;
-		var LnkSta = 0;}
-
-}
-
-setInterval(topImage,7000);
-
-
