66f1e3e7b1eed3c926266b221ba9290073de9457
Krystalit <111550717+Krystalit@users.noreply.github.com>
2022-11-28 17:17:15 +0800

    ver.Alpha 0.1.9

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
-function displayDate(){
-	document.getElementById("dat").innerHTML=Date();
-}
-
 function Search(){
 	var input = prompt("Search on Baidu,if you want to cancel,input nothing and confirm","");
 	 if (input != ""){
@@ -11,10 +7,82 @@ function Search(){
 }
 
 let Blink = document.getElementById("roll");
-let MWlink = document.getElementById("roll1");
 
-var LnkSta = 0;
+Blink.oncu = function(){
 
-Blink.onclick = function(){
-  window.location.assign("https://www.bilibili.com");
+	var r=confirm("To bilibili?");
+	if (r==true){
+    	window.location.assign("https://www.bilibili.com");
+	}
+    	
+}
+
+window.addEventListener("DOMContentLoaded",() => {
+	const clock = new Clock(".clock");
+});
+
+class Clock {
+	constructor(qs) {
+		this.el = document.querySelector(qs);
+		this.time = { a: [], b: [] };
+		this.rollClass = "clock__block--bounce";
+		this.digitsTimeout = null;
+		this.rollTimeout = null;
+		this.mod = 0 * 60 * 1000;
+
+		this.ck();
+	}
+	animateDigits() {
+		const groups = this.el.querySelectorAll("[data-time-group]");
+
+		Array.from(groups).forEach((group,i) => {
+			const { a, b } = this.time;
+
+			if (a[i] !== b[i]) group.classList.add(this.rollClass);
+		});
+
+		clearTimeout(this.rollTimeout);
+		this.rollTimeout = setTimeout(this.removeAnimations.bind(this),900);
+	}
+	displayTime() {
+		Object.keys(this.time).forEach(letter => {
+			const letterEls = this.el.querySelectorAll(`[data-time="${letter}"]`);
+
+			Array.from(letterEls).forEach((el,i) => {
+				el.textContent = this.time[letter][i];
+			});
+		});
+	}
+	ck() {
+		this.upTime();
+		this.displayTime();
+		this.animateDigits();
+		this.u();
+	}
+	removeAnimations() {
+		const groups = this.el.querySelectorAll("[data-time-group]");
+	
+		Array.from(groups).forEach(group => {
+			group.classList.remove(this.rollClass);
+		});
+	}
+	u() {
+		clearTimeout(this.digitsTimeout);
+		this.digitsTimeout = setTimeout(this.ck.bind(this),1e3);	
+	}
+	upTime() {
+		const rawDate = new Date();
+		const date = new Date(Math.ceil(rawDate.getTime() / 1e3) * 1e3 + this.mod);
+		let h = date.getHours();
+		const m = date.getMinutes();
+		const s = date.getSeconds();
+
+		this.time.a = [...this.time.b];
+		this.time.b = [
+			(h < 10 ? `0${h}` : `${h}`),
+			(m < 10 ? `0${m}` : `${m}`),
+			(s < 10 ? `0${s}` : `${s}`),
+		];
+
+	}
 }
