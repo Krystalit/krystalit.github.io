1cad5c4c1fd38982d69cd2435da5987683bd0c8f
Krystalit <111550717+Krystalit@users.noreply.github.com>
2022-11-30 08:11:02 +0800

    ver.Alpha 0.2.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
 function Search(){
 	var input = prompt("Search on Baidu,if you want to cancel,input nothing and confirm","");
 	 if (input != ""){
-	  var web = "https://www.baidu.com/s?ie=utf-8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + input
-      window.location.assign(web);
-	 }
-}
+	  		var web = "https://www.baidu.com/s?ie=utf-8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + input
+     		window.location.assign(web);
+		}
+	}
+
+
+function Searen(){
+	var inpen = prompt("Search on Baidu's encyclopedia,if you want to cancel,input nothing and confirm","");
+		if (inpen != ""){
+			var enweb = "https://baike.baidu.com/item/" + inpen
+	   		window.location.assign(enweb);
+		}
+  }
 
 let Blink = document.getElementById("roll");
 
