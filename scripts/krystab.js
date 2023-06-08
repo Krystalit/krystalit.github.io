15ccd92de24e58e5b1c56836bc41d8fab0f9e46e
Krystalit <qzdreamer@foxmail.com>
2023-06-08 23:08:06 +0800

    ver.Alpha 0.2.14.3
    typo :(
    
    ver.Alpha 0.15
    
    ver.Alpha 0.16
    
    BIG CHANGE JOINED KRYSTAB

---
+++ scripts/krystab.js
@@ -0,0 +1,6 @@
+function krystab() {
+        var input = document.getElementById("theInput")
+        var web = "https://www.baidu.com/s?ie=utf-8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + input.value
+     		window.location.assign(web);
+
+}
