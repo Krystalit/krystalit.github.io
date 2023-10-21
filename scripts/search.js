84809149d71854cc7631b866114d8715c73487e0
Krystalit <qzdreamer@foxmail.com>
2023-10-21 21:57:40 +0800

    10/21 Update

--- scripts/search.js
+++ scripts/search.js
@@ -39,9 +39,40 @@ function Searbi(){
 		}
 	}
 
+function Seardy(){
+	var input = prompt("Search on Douyin,if you want to cancel,input nothing and confirm","");
+	 if (input != ""){
+			  var web = "https://douyin.com/search/" + input
+			 window.location.assign(web);
+			}
+}	
+
+function Searkg(){
+	var input = prompt("Search on Kugou,if you want to cancel,input nothing and confirm","");
+	 if (input != ""){
+			  var web = "https://www.kugou.com/yy/html/search.html#searchType=song&searchKeyWord=" + input
+			 window.location.assign(web);
+			}
+}	
+
+function Searqy(){
+	var input = prompt("Search on QQ Music,if you want to cancel,input nothing and confirm","");
+	 if (input != ""){
+			  var web = "https://y.qq.com/n/ryqq/search?w=" + input
+			 window.location.assign(web);
+			}
+}	
+function Searncm(){
+	var input = prompt("Search on Netease Cloud Music,if you want to cancel,input nothing and confirm","");
+	 if (input != ""){
+			  var web = "https://music.163.com/#/search/m/?s=" + input
+			 window.location.assign(web);
+			}
+}	
 function krystab() {
-    var input = document.getElementById("theInput")
+    var input = document.getElementById("sInput")
     var web = "https://www.baidu.com/s?ie=utf-8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + input.value
+
      	window.location.assign(web);
 
 }
